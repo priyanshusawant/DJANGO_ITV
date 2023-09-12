f7ee3e099940b69a375bd13eedd4924007265456
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-09-12 16:38:19 +0530

    Commit:26

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,6 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=  50)
     Item_desc = models.CharField(max_length = 300)
     item_price = models.IntegerField()
+    item_img = models.CharField(
+        max_length=500,
+        default="https://static.vecteezy.com/system/resources/previews/003/170/825/original/isolated-food-plate-fork-and-spoon-design-free-vector.jpg"
+        )
 
     def __str__(self):
         return self.item_name
