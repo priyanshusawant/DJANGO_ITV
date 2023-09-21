d5ad50194164e331f38748373b0056824d27c2c8
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-09-21 18:08:23 +0530

    Commit:42 (Update)

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -8,14 +8,14 @@ def register(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('food:index')
         
-        else:
-            form = UserCreationForm()
-            
-            context = {
-                'form':form
-            }
-            
-            return render(request, 'users/register.html', context)
+    else:
+        form = UserCreationForm()
+        
+        context = {
+            'form':form
+        }
+        
+        return render(request, 'users/register.html', context)
