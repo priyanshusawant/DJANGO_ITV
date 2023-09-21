73e0fd29fb0d0b6fd5c452a1bb1e225aaa448760
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-09-21 18:22:19 +0530

    Commit:43

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +9,11 @@ def register(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                'Welcome {}, your account has been successfully created'.format(username)
+            )
             form.save()
             return redirect('food:index')
         
